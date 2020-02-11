e89aa61d0eb25d2f94eaf75cc18cbd277c6b20b7
YiDang <dangyi0811@hotmail.com>
2020-02-10 20:37:01 -0800

    Update t.js

--- t.js
+++ t.js
@@ -2,7 +2,13 @@
    var request = new XMLHttpRequest();
    request.open("GET", "https://raw.githack.com/YiDang/pageTest/master/json/story_01.json", false);
    request.send(null)
-alert(request.responseText);
+request.onreadystatechange = function() {
+  if ( request.readyState === 4 && request.status === 200 ) {
+    var my_JSON_object = JSON.parse(request.responseText);
+    console.log(my_JSON_object);
+      alert (my_JSON_object)
+  }
+}
    //var my_JSON_object = JSON.parse(request.responseText);
   // alert (my_JSON_object);
   // alert (my_JSON_object.result[0]);
