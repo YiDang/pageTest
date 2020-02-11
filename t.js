0a47ebe942873f422cd47b39ccf235873fb2a483
YiDang <dangyi0811@hotmail.com>
2020-02-10 20:50:55 -0800

    Update t.js

--- t.js
+++ t.js
@@ -1,36 +1,11 @@
-   alert ("123");
+var request = new XMLHttpRequest();
+request.open("GET", "https://raw.githack.com/YiDang/pageTest/master/json/story_01.json", false);
+request.send(null);
 
- //  request.open("GET", "https://raw.githack.com/YiDang/pageTest/master/json/story_01.json", false);
-function parseUrl(url){
-    var a=document.createElement('a');
-    a.href=url;
-    return {
-      protocol:a.protocol.replace(':',''),
-      hostname:a.hostname,
-      port:a.port,
-      path:a.pathname,
-      query:(()=>{
-        var query=a.search.substr(1);
-        var queryArr=query.split('&');
-        var queryObj={};
-        queryArr.forEach((item,index)=>{
-            var item=item.split('=');
-            var key=item[0];
-            queryObj[key]=item[1];
-        })
-        return queryObj;
-      })(),
-       params:(()=>{
-        var params=a.hash.substr(1);
-        var paramsArr=params.split('#');
-        return paramsArr;
-        
-      })(),
-
-    }
+alert(request.responseText);
+request.onreadystatechange = function() {
+  if ( request.readyState === 4 && request.status === 200 ) {
+    var my_JSON_object = JSON.parse(request.responseText);
+    console.log(my_JSON_object);
+  }
 }
-var urlObj = parseUrl('https://raw.githack.com/YiDang/pageTest/master/json/story_01.json')
-console.log(urlObj)
-   //var my_JSON_object = JSON.parse(request.responseText);
-  // alert (my_JSON_object);
-  // alert (my_JSON_object.result[0]);
