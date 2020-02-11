483849f69242c216aac004eb6790f1106dd70ecf
YiDang <dangyi0811@hotmail.com>
2020-02-10 20:29:47 -0800

    Update t.js

--- t.js
+++ t.js
@@ -1,6 +1,6 @@
    alert ("123");
    var request = new XMLHttpRequest();
-   request.open("GET", "../../data/file.json", false);
+   request.open("GET", "https://raw.githack.com/YiDang/pageTest/master/json/story_01.json", false);
    request.send(null)
    var my_JSON_object = JSON.parse(request.responseText);
    alert (my_JSON_object.result[0]);
